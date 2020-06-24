9060e6456b10e1d387805b4bc315ec7d48b7732a
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-24 15:05:54 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,8 @@ def square_array(numbers)
   numbers = [1,2,3]
   num = []
     array.each do |number|
-    num << numbers ** 2
-    "#{num}"
+    num << number ** 2
+    "#{number}"
   end
+  num
 end
